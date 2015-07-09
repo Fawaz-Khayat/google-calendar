205f386101edc605423594ddad17a095b5780d97
Ed Prince <edward_prince@hotmail.com>
2015-07-09 10:01:25 -0400

    php doc comments added

--- calendar.php
+++ calendar.php
@@ -1,14 +1,14 @@
 
 <?php
- 
- 
-//TO DEBUG UNCOMMENT THESE LINES
-//error_reporting(E_ALL);
-//ini_set("display_errors", 1);
- 
-//INCLUDE THE GOOGLE API PHP CLIENT LIBRARY FOUND HERE
-//https://github.com/google/google-api-php-client
-//DOWNLOAD IT AND PUT IT ON YOUR WEBSERVER IN THE ROOT FOLDER.
+/**
+ * PHP version 5
+ * @category Calendar*
+ * @package Google_Calendar
+ * @author Ed Prince <edward_prince@hotmail.com>
+ * @license http://rem.mit-license.org/ MIT license
+ * @link '/google-api-php-client/src/Google/autoload.php'
+ *
+ */
 require '/var/www/html/google-calendar-list/google-api-php-client/src/Google/autoload.php'; 
  
 $client = new Google_Client();
@@ -18,14 +18,13 @@ $cal = new Google_Service_Calendar($client);
 $calendarId = 'ed.prince5769@gmail.com';
 
 $params = array(
-    'singleEvents' => true, //REQUIRED
+    'singleEvents' => true, 
     'orderBy' => 'startTime',
     'timeMin' => date(DateTime::ATOM)  
 );
 
 $events = $cal->events->listEvents($calendarId, $params); 
-//print_r(json_encode($events->getItems()));
-$calTimeZone = $events->timeZone; //GET THE TZ OF THE CALENDAR
+$calTimeZone = $events->timeZone;
 
 date_default_timezone_set($calTimeZone);
 
@@ -35,9 +34,9 @@ $innerArray = array();
 foreach ($events->getItems() as $event) {  
     
     if (isset ($event->start->date)) { 
-        $array = array("title" => $event->summary, "color" => "red", "description" => $event->description, "start" => $event->start->date, "end" => $event->end->date);
+        $array = array("title" => $event->summary, "description" => $event->description, "start" => $event->start->date, "end" => $event->end->date);
     } else {
-        $array = array("title" => $event->summary, "color" => "red", "description" => $event->description,  "start" => $event->start->dateTime, "end" => $event->end->dateTime); 
+        $array = array("title" => $event->summary, "description" => $event->description,  "start" => $event->start->dateTime, "end" => $event->end->dateTime); 
     }
     array_push($outerArray, $array);
 }
