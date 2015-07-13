ca20b3df261e59ab92e1e3389dbcf833505e3b44
Ed Prince <edward_prince@hotmail.com>
2015-07-13 08:54:01 -0400

    modals added

--- jq-fc.js
+++ jq-fc.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
-  var modal = document.getElementById('modal'); 
-  var title = document.getElementById('title');
-  var date = document.getElementById('date');
-  var description = document.getElementById('description');
-  var modalLine = document.getElementById('modal-line');
+  //var modal = document.getElementById('modal'); 
+  //var title = document.getElementById('title');
+  var date;
+  //var description = document.getElementById('description');
+  var modalBanner = document.getElementById('modal-banner');
+  //var overlay = document.getElementById('overlay');
+  //var hover = false;
   
   $('#calendar').fullCalendar({
-
     header: {
       left: 'prev,next today',
       center: 'title',
       right: 'month,agendaWeek,agendaDay'
     },
     timeFormat: 'H:mm',
+    columnFormat: 'ddd D', //formats date on columns of calendar
     eventLimit: true,
     eventSources: [
     {
@@ -25,22 +27,41 @@ $(document).ready(function() {
     ],
     allDayDefaul: true,
     eventClick: function(calEvent, jsEvent, view) {
+      $('#modal').css('display', 'block');
+      $('#overlay').css('display', 'block');
+      console.log($(this).css('background-color'));
+      //$('modalBanner').css('background-color', $(this).css('background-color'));
+      modalBanner.style.background = $(this).css('background-color');
+      $('#title').text(calEvent.title); 
+      date = moment(calEvent.start).format('MMMM Do YYYY, h:mm:ss a');
+      $('#date').text(date);
+      if (calEvent.description === null) {
+        calEvent.description = ' ';
+      }
+      $('#description').text(calEvent.description);
+      //stopPropagation stops a click event bubbling through the DOM and acting on underlying elements
+      $('#close').click(function(e) {
+        e.stopPropagation();
+        console.log("Event listener running");
+        overlay.style.display = 'none';
+      });
+      $(overlay).click(function(e) {
+        e.stopPropagation();
+        console.log("Overlay clicked");
+        overlay.style.display = 'none';
+      });
+      $('#modal').click(function(e) {
+        e.stopPropagation();
+        $('#overlay').css('display', 'block');
+      });
     },
     eventMouseover: function(calEvent, jsEvent) {
       $(this).css('cursor', 'pointer'); 
-      modal.style.display = 'block';  
-      modalLine.style.background = $(this).css("background-color");
-      title.innerText = calEvent.title; 
-      date.innerText = (moment(calEvent.start).format('MMMM Do YYYY, h:mm:ss a'));
-      description.innerText = calEvent.description;
-      modal.style.left = jsEvent.pageX + "px";
-      modal.style.top = jsEvent.pageY + "px";
+      //modal.style.display = 'block';  
     },
     eventMouseout: function(calEvent) {
-    modal.style.display = 'none';
     }
   });
-
   //Handles list view
   if (document.getElementById('upcoming')) {
     //Append the 3 most upcoming events to a list in a div id="upcoming"
@@ -99,15 +120,6 @@ function displayList(category, data) {
       listDate.appendChild(document.createTextNode(date));
       li.appendChild(listDate);
       li.appendChild(listTime);
-
-      //Uncomment the following to use the description proprty of the event
-      /*
-      var p = document.createElement('p');
-      p.appendChild(document.createTextNode(data[i].description));
-      p.setAttribute('id', 'description');
-      if (data[i].description) {
-        li.appendChild(p);
-      }
-      */
     }
+
 }
